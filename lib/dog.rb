ffc5fcbafb6bddddd542b364491d0d946204e6a1
Jharell Chambers <jharell.chambers@selu.edu>
2019-11-12 20:54:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -30,6 +30,7 @@ def initialize(name:, breed:, id: nil)
      DB[:conn].execute(sql, self.name, self.breed)
      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
    end 
+   return self 
  end
 
 end 
