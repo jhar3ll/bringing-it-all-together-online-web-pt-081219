a949c35a24bbe5cf75bb759bad8780d832eeb8af
Jharell Chambers <jharell.chambers@selu.edu>
2019-11-12 21:17:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -53,7 +53,12 @@ def initialize(name:, breed:, id: nil)
       dog_data = [0]
       dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
     else 
-      
+      dog = self.create(name: name, breed: breed)
+    end 
+    return dog 
+  end 
+  
+  
     
     
 end 
