f52bb611092467114e9908090230b9b5c2c32286
Jharell Chambers <jharell.chambers@selu.edu>
2019-11-12 20:52:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -28,7 +28,7 @@ def initialize(name:, breed:, id: nil)
    else 
      sql = "INSERT INTO dogs (name, breed) VALUES (?, ?)"
      DB[:conn].execute(sql, self.name, self.breed)
-     @id = DB[:conn].execute("SELECT insert_row_id() FROM dogs")[0][0]
+     @id = DB[:conn].execute("SELECT last_insert_row_id() FROM dogs")[0][0]
    end 
  end
 
